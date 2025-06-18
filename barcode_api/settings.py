54589bfd70d2b2213cbf64475276d35f71cf4a35
David Caballero <davidcame124@gmail.com>
2025-06-18 15:27:32 -0500

    update hosts

--- barcode_api/settings.py
+++ barcode_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9uz_r#s4_ofwf7r4)+7!1)b1c3k#l*oz7jz9i3ekyjtb_g+vkt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["lectororc-production.up.railway.app"]
 
 
 # Application definition
